5e1f918ffe150b2284bd7c43ee54d44fd37e9e0e
mikkojamG <mikko.jamia@gofore.com>
2025-01-09 12:46:45 +0200

    deps: implement hds 4.2.0 LINK-2152

--- src/common/components/notificationsContext/NotificationsContext.tsx
+++ src/common/components/notificationsContext/NotificationsContext.tsx
@@ -1,3 +1,4 @@
+import { NotificationSize } from 'hds-react';
 import uniqueId from 'lodash/uniqueId';
 import React, {
   createContext,
@@ -70,7 +71,7 @@ export const NotificationsProvider: FC<PropsWithChildren> = ({ children }) => {
             {...props}
             style={getNotificationStyle()}
             key={props.id}
-            size="default"
+            size={NotificationSize.Medium}
             position="top-right"
             autoClose={true}
             onClose={removeNotification}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,29 +2,24 @@
 
 import '../styles/main.scss';
 
-import { init } from '@socialgouv/matomo-next';
 import {
   HydrationBoundary,
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
+import { CookieConsentContextProvider } from 'hds-react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { appWithTranslation, SSRConfig } from 'next-i18next';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { AccessibilityNotificationProvider } from '../common/components/accessibilityNotificationContext/AccessibilityNotificationContext';
 import { NotificationsProvider } from '../common/components/notificationsContext/NotificationsContext';
 import CookieConsent from '../domain/app/cookieConsent/CookieConsent';
 import PageLayout from '../domain/app/layout/pageLayout/PageLayout';
-
-const MATOMO_ENABLED = process.env.NEXT_PUBLIC_MATOMO_ENABLED;
-const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
-const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
-const MATOMO_JS_TRACKER_FILE = process.env.NEXT_PUBLIC_MATOMO_JS_TRACKER_FILE;
-const MATOMO_PHP_TRACKER_FILE = process.env.NEXT_PUBLIC_MATOMO_PHP_TRACKER_FILE;
+import useCookieConsentSettings from '../hooks/useCookieConsentSettings';
 
 type Props = {
   dehydratedState?: unknown;
@@ -37,36 +32,29 @@ const MyApp = ({
 }: AppProps<Props>) => {
   const [queryClient] = React.useState(() => new QueryClient());
 
-  useEffect(() => {
-    if (MATOMO_ENABLED === 'true' && MATOMO_URL && MATOMO_SITE_ID) {
-      init({
-        jsTrackerFile: MATOMO_JS_TRACKER_FILE,
-        phpTrackerFile: MATOMO_PHP_TRACKER_FILE,
-        url: MATOMO_URL,
-        siteId: MATOMO_SITE_ID,
-      });
-    }
-  }, []);
+  const cookieConsentProps = useCookieConsentSettings();
 
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <AccessibilityNotificationProvider>
-        <NotificationsProvider>
-          <SessionProvider session={session} refetchInterval={30}>
-            <QueryClientProvider client={queryClient}>
-              <HydrationBoundary state={pageProps.dehydratedState}>
-                <CookieConsent />
-                <PageLayout {...pageProps}>
-                  <Component {...pageProps} />
-                </PageLayout>
-              </HydrationBoundary>
-            </QueryClientProvider>
-          </SessionProvider>
-        </NotificationsProvider>
-      </AccessibilityNotificationProvider>
+      <CookieConsentContextProvider {...cookieConsentProps}>
+        <AccessibilityNotificationProvider>
+          <NotificationsProvider>
+            <SessionProvider session={session} refetchInterval={30}>
+              <QueryClientProvider client={queryClient}>
+                <HydrationBoundary state={pageProps.dehydratedState}>
+                  <CookieConsent />
+                  <PageLayout {...pageProps}>
+                    <Component {...pageProps} />
+                  </PageLayout>
+                </HydrationBoundary>
+              </QueryClientProvider>
+            </SessionProvider>
+          </NotificationsProvider>
+        </AccessibilityNotificationProvider>
+      </CookieConsentContextProvider>
     </>
   );
 };
