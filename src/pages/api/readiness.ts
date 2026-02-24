2be0af57c81b87265d3374efe64132c48c72bbfe
mikkojamG <mikko.jamia@gofore.com>
2026-02-24 13:40:54 +0200

    feat: readiness endpoint KEH-228

--- src/pages/api/readiness.ts
+++ src/pages/api/readiness.ts
@@ -1,8 +1,12 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<string>
-) {
-  res.setHeader("Content-Type", "text/plain").status(200).send("OK")
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const packageVersion = process.env.APP_VERSION ?? '';
+  const release = process.env.NEXT_PUBLIC_SENTRY_RELEASE ?? '';
+
+  res.status(200).json({
+    status: 'ok',
+    release,
+    packageVersion,
+  });
 }
