64ca6bf300e5dfa366b5ee9040d0c3555530ac08
mikkojamG <mikko.jamia@gofore.com>
2026-08-26 14:01:34 +0300

    refactor: remove extra type Refs: LINK-2602

--- src/types.ts
+++ src/types.ts
@@ -94,8 +94,6 @@ export type RefreshTokenResponse = {
   id_token: string;
 };
 
-export type ApiTokenResponse = RefreshTokenResponse;
-
 export type MutationCallbacks<SuccessResponseType = string> = {
   onError?: (error: any) => void;
   onSuccess?: (data?: SuccessResponseType) => void;
