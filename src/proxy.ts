4ee5e9ce060849ae07d237a6e640376228d00f89
mikkojamG <mikko.jamia@gofore.com>
2026-09-21 14:09:27 +0300

    chore: rename middleware to proxy Refs: LINK-2504

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server'
- 
+
 const cspReportingEndpoints = process.env.NEXT_CSP_REPORTING_ENDPOINTS;
 const cspPolicy = process.env.NEXT_CSP_POLICY;
 // default to report only, unless really set to 'false'
 const cspReportOnly = !(process.env.NEXT_CSP_REPORT_ONLY === 'false')
 const cspHeader = cspReportOnly ? 'Content-Security-Policy-Report-Only' : 'Content-Security-Policy';
 
-export function middleware() {
+export function proxy() {
 
   const response = NextResponse.next()
 
