7cc525d70e8fa59ce92aefc2bc336a74826b3bac
mikkojamG <mikko.jamia@gofore.com>
2026-09-22 12:06:48 +0300

    fix: proxy sonar maintainability Refs: LINK-2504

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,15 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
 const cspReportingEndpoints = process.env.NEXT_CSP_REPORTING_ENDPOINTS;
 const cspPolicy = process.env.NEXT_CSP_POLICY;
 // default to report only, unless really set to 'false'
-const cspReportOnly = !(process.env.NEXT_CSP_REPORT_ONLY === 'false')
-const cspHeader = cspReportOnly ? 'Content-Security-Policy-Report-Only' : 'Content-Security-Policy';
+const cspReportOnly = process.env.NEXT_CSP_REPORT_ONLY !== 'false';
+const cspHeader = cspReportOnly
+  ? 'Content-Security-Policy-Report-Only'
+  : 'Content-Security-Policy';
 
 export function proxy() {
-
-  const response = NextResponse.next()
+  const response = NextResponse.next();
 
   if (cspPolicy) {
     response.headers.set(cspHeader, cspPolicy);
@@ -17,10 +18,10 @@ export function proxy() {
     }
   }
 
-  return response
+  return response;
 }
 
 // Only apply CSP to HTML pages (exclude files with extensions)
 export const config = {
   matcher: '/((?!.*\\.).*)',
-}
+};
