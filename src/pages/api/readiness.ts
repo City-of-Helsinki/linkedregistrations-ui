4829966cb15671ba94d8ae99a757b9737134a0d9
mikkojamG <mikko.jamia@gofore.com>
2026-09-23 15:57:40 +0300

    fix: readiness use version Refs: RATY-355

--- src/pages/api/readiness.ts
+++ src/pages/api/readiness.ts
@@ -3,12 +3,12 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import getEnvValue from '../../utils/getEnvValue';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const packageVersion = process.env.APP_VERSION ?? '';
+  const appVersion = process.env.APP_VERSION ?? '';
   const release = getEnvValue('NEXT_PUBLIC_SENTRY_RELEASE') ?? '';
 
   res.status(200).json({
     status: 'ok',
     release,
-    packageVersion,
+    version: appVersion,
   });
 }
